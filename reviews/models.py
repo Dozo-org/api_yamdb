00462e9cf2a8b968eecc16f363b88b4e78bc7a30
Eugen Hoppe <eugen.hoppe@yandex.com>
2021-04-24 16:27:57 +0200

    new branch

--- reviews/models.py
+++ reviews/models.py
@@ -3,12 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from title_category_genre.models import Title
 
-#class Title(models.Model):
-    #name = models.TextField()
-    
-    #def __str__(self):
-        #return self.name
-
 
 
 class Review(models.Model):
