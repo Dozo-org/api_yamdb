3e22c498cace1f2530ef3dc1c8ddca9be818896f
Roman Rogachev <75900827+Dozo-org@users.noreply.github.com>
2021-04-25 22:05:45 +0300

    Develop (#4)
    * First
    
    * Admin, models, serializer, urls, views, api_yamdb/settings
    
    * First api structure ready
    
    * Refactring
    
    * First commit
    
    * feature/Dev_B
    
    * feature/dev_B
    
    * feature/dev_B
    
    * feature/dev_B
    
    * Refactoring and implement user model
    
    * feature/dev_B
    
    * User model
    
    * App Auth users is working
    
    * Users
    
    * Test
    
    * Test
    
    * new branch
    
    * Test
    
    * Tests started be green
    
    * Test passed 17 failed 14
    
    * passed all tests
    
    * v1.0
    
    * feature/dev_B
    
    * Сделал правку
    
    * Test
    
    * Test
    
    * The first iteration
    
    Co-authored-by: Eugen Hoppe <eugen.hoppe@yandex.com>
    Co-authored-by: TimurAtambaev <atambaev86@gmail.com>

---
+++ title_category_genre/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ManyToManyField(Genre, related_name='genres')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 related_name='categories', blank=True,
+                                 null=True)
+
+    class Meta:
+        ordering = ['-year']
+
+    def __str__(self):
+        return self.name
