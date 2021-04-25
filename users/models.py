52cac9eab838929e4f739c96155efdd4941588d8
Роман Рогачев <rogachew.r@yandex.ru>
2021-04-25 21:01:49 +0300

    Test

--- users/models.py
+++ users/models.py
@@ -1,45 +1,17 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractUser
-
-
-# class CustomUser(AbstractUser):
-#     ROLE_CHOICES = (
-#         ('user', 'user'),
-#         ('moderator', 'moderator'),
-#         ('admin', 'admin'),
-#     )
-#     username = models.CharField(max_length = 30, unique=True)
-#     email = models.EmailField(max_length = 254, unique=True)
-#     role = models.CharField(max_length=9, choices=ROLE_CHOICES, default='user')
-#     first_name = models.CharField(max_length=30, blank=True)
-#     last_name = models.CharField(max_length=30, blank=True)
-#     bio = models.CharField(max_length=254, blank=True)
-#     confirmation_code =models.CharField(max_length=30, blank=True)
-
-import uuid
-
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(max_length=30, blank=True,)
-    email = models.EmailField(unique=True,)
-
-    class UserRole:
-        USER = 'user'
-        ADMIN = 'admin'
-        MODERATOR = 'moderator'
-        choices = [
-            (USER, 'user'),
-            (ADMIN, 'admin'),
-            (MODERATOR, 'moderator'),
-        ]
-
-    role = models.CharField(max_length=9, choices=UserRole.choices, default=UserRole.USER,)
-    confirmation_code = models.UUIDField(default=uuid.uuid4, editable=False,)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-
+    ROLE_CHOICES = (
+        ('user', 'user'),
+        ('moderator', 'moderator'),
+        ('admin', 'admin'),
+    )
+    username = models.CharField(max_length = 30, unique=True)
+    email = models.EmailField(max_length = 254, unique=True)
+    role = models.CharField(max_length=9, choices=ROLE_CHOICES, default='user')
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    bio = models.CharField(max_length=254, blank=True)
+    confirmation_code =models.CharField(max_length=30, blank=True)
--- users/permission.py
+++ users/permission.py
@@ -19,7 +19,7 @@ class IsAdmin(permissions.BasePermission):
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS or
-                request.user.is_staff or
-                request.user.role == request.user.UserRole.ADMIN
+            request.method in permissions.SAFE_METHODS or
+            request.user.is_staff or
+            request.user.role == 'admin'
         )
