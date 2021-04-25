61a54891c310b8b90c907e875414f2326637d703
Роман Рогачев <rogachew.r@yandex.ru>
2021-04-25 22:17:23 +0300

    The first iteration

--- users/urls.py
+++ users/urls.py
@@ -7,14 +7,14 @@ router = DefaultRouter()
 router.register('users', UserViewSet, basename='UserView')
 
 urlpatterns = [
-     path('v1/auth/email/',
-          CreateUser.as_view(),
-          name='user_creat'),
-     path('v1/auth/token/',
-          MyTokenObtainPairView.as_view(),
-          name='token_obtain_pair'),
-     path('v1/users/me/',
-          UserViewMe.as_view(),
-          name='user_me'),
-     path('v1/', include(router.urls)),
+    path('v1/auth/email/',
+         CreateUser.as_view(),
+         name='user_creat'),
+    path('v1/auth/token/',
+         MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('v1/users/me/',
+         UserViewMe.as_view(),
+         name='user_me'),
+    path('v1/', include(router.urls)),
 ]
